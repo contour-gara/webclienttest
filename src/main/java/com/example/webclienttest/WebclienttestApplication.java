2d6b50a0478e12482823dfaa3df213cfcb904f84
Contour gara <the.rocker106@outlook.jp>
2023-07-24 02:36:25 +0900

    first commit

---
+++ src/main/java/com/example/webclienttest/WebclienttestApplication.java
@@ -0,0 +1,13 @@
+package com.example.webclienttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebclienttestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebclienttestApplication.class, args);
+	}
+
+}
